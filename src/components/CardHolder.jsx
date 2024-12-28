59362bff27a7daf3f516b11d1c392b7cb1cd683d
Finn <112250336+finn200102@users.noreply.github.com>
2024-12-28 18:23:08 +0100

    feat: Implement shuffel after each click

--- src/components/CardHolder.jsx
+++ src/components/CardHolder.jsx
@@ -6,6 +6,25 @@ import "../styles/CardHolder.css";
 function CardHolder({ onClick, count }) {
   const [imageUrls, setImageUrls] = useState([]);
 
+  const shuffle = (array) => {
+    // shuffel array with Fisher-Yates (Knuth) shuffle algorithm
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
+  useEffect(() => {
+    setImageUrls((prevUrls) => {
+      if (prevUrls.length > 0) {
+        return shuffle(prevUrls);
+      }
+      return prevUrls;
+    });
+  }, [onClick]);
+
   useEffect(() => {
     const fetchUrls = async () => {
       try {
